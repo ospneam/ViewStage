fc1599796b38e591a7850f33a9cd2aecb160df4d
ospneam <2140857671@qq.com>
2026-03-14 09:25:12 +0800

    优化笔体验,去除多余控制选项，更新版本号v0.7.0

--- src/wasm-processor.js
+++ src/wasm-processor.js
@@ -1,6 +1,6 @@
 /**
  * WASM 点处理模块
- * 提供高性能的点优化、距离计算等功能
+ * 提供高性能的路径平滑和视口裁剪功能
  */
 
 class WasmPointProcessor {
@@ -30,15 +30,12 @@ class WasmPointProcessor {
 
     async _loadWasm() {
         try {
-            const { default: init, process_stroke_points, smooth_path, collect_points, batch_process_draw_commands, cull_strokes_by_viewport } = await import('./wasm/wasm_viewstage.js');
+            const { default: init, smooth_path, cull_strokes_by_viewport } = await import('./wasm/wasm_viewstage.js');
             
             await init();
             
             this.wasmModule = {
-                process_stroke_points,
                 smooth_path,
-                collect_points,
-                batch_process_draw_commands,
                 cull_strokes_by_viewport
             };
             
@@ -52,17 +49,6 @@ class WasmPointProcessor {
         }
     }
 
-    async processStrokePoints(points, config) {
-        await this.load();
-        const request = {
-            points,
-            config
-        };
-        const requestJson = JSON.stringify(request);
-        const resultJson = this.wasmModule.process_stroke_points(requestJson);
-        return JSON.parse(resultJson);
-    }
-
     async smoothPath(points, smoothness, algorithm = 'bezier') {
         await this.load();
         const request = {
@@ -72,34 +58,6 @@ class WasmPointProcessor {
         };
         const requestJson = JSON.stringify(request);
         const resultJson = this.wasmModule.smooth_path(requestJson);
-        return JSON.parse(resultJson);
-    }
-
-    async collectPoints(points, config, lastTime, lastX, lastY) {
-        await this.load();
-        const currentTime = Date.now();
-        const request = {
-            points,
-            config,
-            lastTime: lastTime,
-            lastX: lastX,
-            lastY: lastY,
-            currentTime: currentTime
-        };
-        const requestJson = JSON.stringify(request);
-        const resultJson = this.wasmModule.collect_points(requestJson);
-        return JSON.parse(resultJson);
-    }
-
-    async batchProcessDrawCommands(commands, minDistance, maxBatchSize) {
-        await this.load();
-        const request = {
-            commands,
-            min_distance: minDistance,
-            max_batch_size: maxBatchSize
-        };
-        const requestJson = JSON.stringify(request);
-        const resultJson = this.wasmModule.batch_process_draw_commands(requestJson);
         return JSON.parse(resultJson);
     }
 
